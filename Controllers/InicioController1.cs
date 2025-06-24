4c481a6d5d9aea1c764cb5868f53636be96ef804
Tiffa Rodriguez <tiffanys2703@gmail.com>
2025-06-24 14:30:03 -0600

    Hola

---
+++ Controllers/InicioController1.cs
@@ -0,0 +1,9 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+public class InicioController : Controller
+{
+    public IActionResult Inicio()
+    {
+        return View(); // Buscará Views/Inicio/inicio.cshtml
+    }
+}
